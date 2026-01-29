3f4c0f7cac2a1822c882176e68da4bde8ab53977
0jann-exe <r4zan.maulana@gmail.com>
2026-01-29 07:15:20 +0700

    tugas conditional dan looping : done

---
+++ php-2 sb/looping.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Looping</title>
+
+</head>
+
+<body>
+
+    <h1>Berlatih Looping</h1>    
+     <?php 
+
+        echo "<h3>Soal No 1 Looping I Love PHP</h3>";
+
+        echo "LOOPING 1 <br><br>";
+        for($i=2; $i < 21; $i+=2) {
+            echo $i . " - I Love PHP <br>";
+        }
+
+        echo "<br>";
+        echo "LOOPING 2 <br><br>";
+
+        $x=20;
+        while($x >= 2){
+            echo $x . " - I Love PHP <br>";
+            $x-=2;
+        }
+
+        /* 
+
+            Soal No 1 
+
+            Looping I Love PHP
+
+            Lakukan Perulangan (boleh for/while/do while) sebanyak 20 iterasi. Looping terbagi menjadi dua: Looping yang pertama Ascending (meningkat) 
+
+            dan Looping yang ke dua menurun (Descending).             
+Output: 
+
+            LOOPING PERTAMA
+
+            2 - I Love PHP
+
+            4 - I Love PHP
+
+            6 - I Love PHP
+
+            8 - I Love PHP
+
+            10 - I Love PHP
+
+            12 - I Love PHP
+
+            14 - I Love PHP
+
+            16 - I Love PHP
+
+            18 - I Love PHP
+
+            20- I Love PHP
+
+            LOOPING KEDUA
+
+            20 - I Love PHP
+
+            18 - I Love PHP
+
+            16 - I Love PHP
+
+            14 - I Love PHP
+
+            12 - I Love PHP
+
+            10 - I Love PHP
+
+            8 - I Love PHP
+
+            6 - I Love PHP
+
+            4 - I Love PHP
+
+            2 - I Love PHP
+
+        */
+
+
+        /* 
+
+            Soal No 2
+
+            Loop Associative Array
+
+            Terdapat data items dalam bentuk array dimensi. Buatlah data tersebut ke dalam bentuk Array Asosiatif. 
+
+            Setiap item memiliki key yaitu : id, name, price, description, source. 
+
+            
+
+            Output: 
+
+            Array ( [id] => 001 [name] => Keyboard Logitek [price] => 60000 [description] => Keyboard yang mantap untuk kantoran [source] => logitek.jpeg ) 
+
+            Array ( [id] => 002 [name] => Keyboard MSI [price] => 300000 [description] => Keyboard gaming MSI mekanik [source] => msi.jpeg ) 
+
+            Array ( [id] => 003 [name] => Mouse Genius [price] => 50000 [description] => Mouse Genius biar lebih pinter [source] => genius.jpeg ) 
+
+            Array ( [id] => 004 [name] => Mouse Jerry [price] => 30000 [description] => Mouse yang disukai kucing [source] => jerry.jpeg )             
+        Jangan ubah variabel $items        */
+
+        echo "<br>";
+        echo "<br>";
+
+        $items = [
+
+            ['001', 'Keyboard Logitek', 60000, 'Keyboard yang mantap untuk kantoran', 'logitek.jpeg'], 
+
+            ['002', 'Keyboard MSI', 300000, 'Keyboard gaming MSI mekanik', 'msi.jpeg'],
+
+            ['003', 'Mouse Genius', 50000, 'Mouse Genius biar lebih pinter', 'genius.jpeg'],
+
+            ['004', 'Mouse Jerry', 30000, 'Mouse yang disukai kucing', 'jerry.jpeg']
+
+        ];
+
+        foreach($items as $key => $value)  {
+            $dataItem = array(
+                "id" => $value[0],
+                "name" => $value[1],
+                "price" => $value[2],
+                "description" => $value[3],
+                "source" => $value[4]
+            );
+
+            print_r($dataItem);
+            echo "<br>";
+        }
+
+        // Output: 
+
+
+        echo "<h3>Soal No 4 Asterix </h3>";
+
+        /* 
+
+            Soal No 3
+
+            Asterix 5x5
+
+            Tampilkan dengan looping dan echo agar menghasilkan kumpulan bintang dengan pola seperti berikut: 
+
+            Output: 
+
+            * 
+
+            * * 
+
+            * * * 
+
+            * * * * 
+
+            * * * * *
+
+        */
+
+        for($v=1; $v<=5; $v++){
+            for($y=1; $y<=$v; $y++) {
+                echo "*";
+            }
+            echo "<br>";
+        }       
+
+    ?></body>
+
+</html>
